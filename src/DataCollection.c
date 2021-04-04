22fa4ec36e3996524c3438098e2d512292b2153a
2237montala <anthony.montalbano@marquette.edu>
2021-04-04 10:21:49 -0500

    Changed free fall dection to be something new

--- src/DataCollection.c
+++ src/DataCollection.c
@@ -10,7 +10,7 @@ ShockSensorDataStruct newConvertedDataSample;
 ShockSensorDataStruct lastestFilteredData;
 
 // Create a fifo for holding the data
-_fff_declare(ShockSensorDataStruct,ShockSenorDataFifo,DATA_BUFFER_LEN);
+_fff_declare(ShockSensorDataStruct,ShockSensorDataFifo,DATA_BUFFER_LEN);
 
 // Static function declarations
 static bool collectDataFromSensors();
@@ -24,13 +24,15 @@ void initializeCollectData() {
     memset(&lastestFilteredData,0x0,sizeof(lastestFilteredData));
 
     // Initialize the fifo
-    _fff_init(ShockSenorDataFifo);
+    _fff_init(ShockSensorDataFifo);
 }
 
 static bool collectDataFromSensors() {
     // Temporary struct for holding accelerometer data
     static struct Lis3dhDataStruct accelSensorData;
     static uint32_t currLinearPosTicks;
+    static uint32_t samplesInFreefall = 0;
+    static uint32_t samplesNotInFreefall = 0;
     bool succeed = false;
 
     // Read accelerometer
@@ -58,11 +60,28 @@ static bool collectDataFromSensors() {
             break;  
     }
 
+    // Check if the sample was within the free fall threshold
     if(accelToCheck != INFINITY && accelToCheck > FREE_FALL_G_THRESHOLD_NEG && accelToCheck < FREE_FALL_G_THRESHOLD_POS) {
         // We could be in free fall so we should check
         // TODO: Implement true free fall detection
-        newConvertedDataSample.inFreefall = true;
+        samplesInFreefall++;
+        if(samplesInFreefall > SAMPLES_BEFORE_VALID_FREE_FALL) {
+            newConvertedDataSample.inFreefall = true;
+        }
+
+        
+    } else if (samplesInFreefall > 0 && samplesNotInFreefall < SAMPLES_BEFORE_VALID_FREE_FALL) {
+        // The sample wasn't in the free fall threshold but we might still be in free fall
+        // This helps remove jitter in the free fall state
+        samplesNotInFreefall++;
+    } else {
+        // Last SAMPLES_BEFORE_VALID_FREE_FALL samples were not in free fall so reset our state
+        // to not being in free fall
+        newConvertedDataSample.inFreefall = false;
+        samplesNotInFreefall = 0;
+        samplesInFreefall = 0;
     }
+    
 
     // TODO: Add correct encoder tick calculating
     // Read in encoder ticks
@@ -84,12 +103,12 @@ bool collectData() {
     }
 
     // If fifo is full then pop off an element
-    if(_fff_is_full(ShockSenorDataFifo)) {
-        _fff_remove(ShockSenorDataFifo,1);
+    if(_fff_is_full(ShockSensorDataFifo)) {
+        _fff_remove(ShockSensorDataFifo,1);
     }
 
     // Add new sample to the fifo
-    _fff_write_lite(ShockSenorDataFifo,newConvertedDataSample);
+    _fff_write_lite(ShockSensorDataFifo,newConvertedDataSample);
     
     return true;
 }
@@ -113,21 +132,11 @@ void filterData() {
     for(int i = 0; i < DATA_BUFFER_LEN; i++) {
         // Sum accels
         for(int q = 0; q < NUMBER_OF_AXIS; q++) {
-            lastestFilteredData.accels[q] += _fff_peek(ShockSenorDataFifo,i).accels[q]; 
+            lastestFilteredData.accels[q] += _fff_peek(ShockSensorDataFifo,i).accels[q]; 
         }
 
         // Sum linear position
-        lastestFilteredData.linearPos += _fff_peek(ShockSenorDataFifo,i).linearPos;
-
-        // TODO: do something with free fall here
-        // // Sum all instances that are in between valid free fall values
-        // if(consecutiveFreefall && _fff_peek(ShockSenorDataFifo,i).inFreefall) { {
-        //         currValidFreefalls++;
-        //     } else {
-        //         consecutiveFreefall = false;
-        //     }
-        // }
-        
+        lastestFilteredData.linearPos += _fff_peek(ShockSensorDataFifo,i).linearPos;        
     }
 
     for(int i = 0; i < NUMBER_OF_AXIS; i++) {
@@ -135,23 +144,15 @@ void filterData() {
     }
 
     lastestFilteredData.linearPos /= DATA_BUFFER_LEN;
-
-    // TODO: do something with free fall here
-    // // Maybe count the number of continous free fall values
-    // totalValidFreefalls += currValidFreefalls;
-
-    // if(currValidFreefalls < TIME_BEFORE_FREE_FALL_RESET) {
-    //     // Free fall reset
-    //     lastestFilteredData.inFreefall = false;
-    //     totalValidFreefalls = 0;
-    // } else if(totalValidFreefalls >= TIME_BEFORE_VALID_FREE_FALL) {
-    //     lastestFilteredData.inFreefall = true;
-    // }
-    lastestFilteredData.inFreefall = false;
+    
+    // Set free fall state which was averaged out in the last samples
+    // so no filtering has to be done here. If the system is changing free fall states
+    // then this variable will be a few ms old. At most 5ms
+    lastestFilteredData.inFreefall = _fff_peek(ShockSensorDataFifo,0);
 }
 
 ShockSensorDataStruct* getMostRecentSensorData() {
-    return &(_fff_peek(ShockSenorDataFifo,0));
+    return &(_fff_peek(ShockSensorDataFifo,0));
 }
 
 ShockSensorDataStruct* getMostRecentFilteredSensorData() {
